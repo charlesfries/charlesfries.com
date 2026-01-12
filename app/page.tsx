a16a345ce0ba6e0e0396bc6e1d22c1c7c4701c75
Charles Fries <charliefries@icloud.com>
2026-01-12 15:57:38 -0800

    Add image, heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,17 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="special">Charles Fries</h1>
+
+        <Image
+          className="rounded-full"
+          src="https://avatars.githubusercontent.com/u/2275005?v=4"
+          alt="Charles Fries"
+          width={200}
+          height={200}
+          priority
+        />
+
         <Image
           className="dark:invert"
           src="/next.svg"
