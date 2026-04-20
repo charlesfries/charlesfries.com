a46fee87a8ee91cf1a987a0869798ac30a572206
Charles Fries <charliefries@icloud.com>
2026-04-20 00:41:33 -0700

    WIP

--- app/routes/articles.ts
+++ app/routes/articles.ts
@@ -1,3 +1,22 @@
 import Route from '@ember/routing/route';
 
-export default class ArticlesRoute extends Route {}
+export default class ArticlesRoute extends Route {
+  model() {
+    return [
+      {
+        title: 'How I Built My Personal Website with Ember.js',
+        createdAt: new Date(),
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        url: 'https://www.google.com/',
+      },
+      {
+        title: 'How I Built My Personal Website with Ember.js',
+        createdAt: new Date(),
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        url: 'https://www.google.com/',
+      },
+    ];
+  }
+}
--- app/templates/articles.gts
+++ app/templates/articles.gts
@@ -1,4 +1,5 @@
 import type { TOC } from '@ember/component/template-only';
+import { formatDate } from 'ember-intl';
 import { pageTitle } from 'ember-page-title';
 
 interface ArticlesSignature {
@@ -11,5 +12,24 @@ interface ArticlesSignature {
 <template>
   {{pageTitle "Articles"}}
 
-  Articles
+  <div class="flex flex-col gap-8 max-w-2xl mx-auto">
+    {{#each @model as |article|}}
+      <div>
+        <h2 class="text-xl font-bold mb-2">{{article.title}}</h2>
+        <p class="text-neutral-500 dark:text-neutral-400 mb-2">
+          {{formatDate article.createdAt dateStyle="long"}}
+        </p>
+        <p class="mb-2">{{article.description}}</p>
+        <a
+          href={{article.url}}
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-blue-600 dark:text-blue-400 underline"
+        >
+          Read article
+        </a>
+      </div>
+
+    {{/each}}
+  </div>
 </template> satisfies TOC<ArticlesSignature>;
