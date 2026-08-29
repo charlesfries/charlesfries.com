1aa4da70a9312612c82fc737d7afa8a857271aed
Charles Fries <charliefries@icloud.com>
2026-08-28 17:51:02 -0700

    warn

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -5,5 +5,19 @@ module.exports = {
       url: ['http://localhost:4173/'],
       startServerReadyPattern: 'Build successful',
     },
+    assert: {
+      assertions: {
+        'first-contentful-paint': 'warn',
+        'errors-in-console': 'warn',
+        'font-display': 'warn',
+        'image-delivery-insight': 'warn',
+        'network-dependency-tree-insight': 'warn',
+        'unsized-images': 'warn',
+        'unused-javascript': 'warn',
+        'uses-rel-preconnect': 'warn',
+        'uses-responsive-images': 'warn',
+        'valid-source-maps': 'warn',
+      },
+    },
   },
 };
