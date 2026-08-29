379d7a09b7e6527129406bf6497af835aa697fc8
Charles Fries <charliefries@icloud.com>
2026-08-28 17:39:07 -0700

    preview

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'pnpm dev',
+      startServerCommand: 'pnpm preview',
       url: ['http://localhost:8888/'],
       startServerReadyPattern: 'Build successful',
     },
